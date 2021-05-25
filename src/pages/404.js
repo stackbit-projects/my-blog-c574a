eff955387886c59a7220cfaa965553112f125047
Stackbit Code Editor <projects@stackbit.com>
2021-05-25 01:51:57 +0000

    404.js: changed

--- src/pages/404.js
+++ src/pages/404.js
@@ -8,8 +8,8 @@ import SEO from "../components/seo"
 
 const NotFoundPage = () => (
   <Layout>
-    <SEO title="404: Not found" />
-    <Styled.h1>NOT FOUND</Styled.h1>
+    <SEO title="404: Não encontrado" />
+    <Styled.h1>Não Encontrado</Styled.h1>
     <Styled.p>
       You just hit a route that doesn&#39;t exist... the sadness.
     </Styled.p>
